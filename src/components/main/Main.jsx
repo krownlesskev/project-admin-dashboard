66ca86ce212d3179d11dfaa89185cd85fa3da19d
Kevin Sana <kevin.sana@outlook.com>
2022-05-05 00:31:23 -0500

    made drawer and header top responsive

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -8,10 +8,13 @@ const Main = () => {
             <div className='header'>
                 <div className='header-top'>
                     <div className='header-top-left'>
-                        <MdOutlineSearch />
+                        <MdOutlineSearch size='2em' />
+                        <input type="text" />
                     </div>
                     <div className='header-top-right'>
-
+                        <MdOutlineNotificationsActive size='2em' />
+                        <img src="https://via.placeholder.com/50" alt="placeholder" />
+                        <span>Kevin Sana</span>
                     </div>
                 </div>
                 <div className='header-bottom'>
