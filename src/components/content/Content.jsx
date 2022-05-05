e2497903f78ec8c033904891baa2fa873c49e3a5
Kevin Sana <kevin.sana@outlook.com>
2022-05-04 20:27:13 -0500

    added box shadow and bg color for main

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Content.styles.scss';
 import Header from './header/Header';
+import Main from './main/Main';
 
 const Content = () => {
   return (
     <div className='content-container'>
       <Header />
+      <Main />
     </div>
   );
 };
---
+++ src/components/content/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.styles.scss';
+
+const Main = () => {
+    return (
+        <div className='main-container'>
+        </div>
+    );
+};
+
+export default Main;
