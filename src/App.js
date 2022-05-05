5730b958780016b803df2f6695151ce61db00a28
Kevin Sana <kevin.sana@outlook.com>
2022-05-04 23:55:00 -0500

    reworking components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './app.styles.scss';
-import Content from './components/content/Content';
 import Drawer from './components/drawer/Drawer';
 
 function App() {
   return (
     <div className='app-container'>
       <Drawer />
-      <Content />
     </div>
   );
 }
