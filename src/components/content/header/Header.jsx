f621710c8a72a7a94ec90bda195858571b9a219a
Kevin Sana <kevin.sana@outlook.com>
2022-05-04 20:05:07 -0500

    finished styling header-top-right

--- src/components/content/header/Header.jsx
+++ src/components/content/header/Header.jsx
@@ -14,12 +14,18 @@ const Header = () => {
                     <input type="text" />
                 </div>
                 <div className='header-top-right'>
-                    <MdOutlineNotificationsActive />
+                    <MdOutlineNotificationsActive size='2em' />
                     <img src="https://via.placeholder.com/50" />
                     <span>Kevin Sana</span>
                 </div>
             </div>
             <div className='header-bottom'>
+                <div className='header-bottom-left'>
+
+                </div>
+                <div className='header-bottom-right'>
+
+                </div>
             </div>
         </div>
     );
